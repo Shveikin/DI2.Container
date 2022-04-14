705216cd30608936c31609ab39f9f2e9d55163f8
picus23 <picus.2022@mail.ru>
2022-04-14 09:23:45 +0300

    mp

--- src/Container.php
+++ src/Container.php
@@ -2,59 +2,66 @@
 
 namespace DI2;
 
-class ContainerManager {
+
+
+class MP {
     static $container = false;
     private $containers = [];
     private $reg = [];
 
-    static function container(){
+    static function GET($class = false, $alias = false){
         if (self::$container==false)
             self::$container = new self();
+
+        if ($class!=false){
+            return self::$container->class($class, $alias);
+        }
 
         return self::$container;
     }
 
-    function class($class){
-        if (!isset($this->reg[$class])){
-            if (!isset($this->containers[$class])){
-                $this->reg[$class] = true;
+    function class($class, $alias = false){
+        if ($alias==false) $alias = $class;
 
-                $tempElement = new $class(function($el){
-                    $class = get_class($el);
-                    $this->containers[$class] = $el;
+        if (!isset($this->reg[$alias])){
+            if (!isset($this->containers[$alias])){
+                $this->reg[$alias] = true;
+
+                $tempElement = new $class(function($el) use ($alias){
+                    $this->containers[$alias] = $el;
                 });
 
                 if (method_exists($tempElement, '__parent')){
                     $tempElement->__parent();
                 }
 
-                if (!isset($this->containers[$class])){
-                    $this->containers[$class] = $tempElement;
+                if (!isset($this->containers[$alias])){
+                    $this->containers[$alias] = $tempElement;
                 }
             }
         } else {
-            if (!isset($this->containers[$class]))
+            if (!isset($this->containers[$alias]))
                 throw new \Exception(" $class не реальзует функцию \$super(\$this) в __construct ", 1);
         }
 
-        return $this->containers[$class];
+        return $this->containers[$alias];
     }
 }
 
 trait Container {
     static function main(){
-        return ContainerManager::container()->class(static::class);
+        return MP::GET(static::class);
     }
 
     static function __callStatic($func, $arguments){
-        $class = ContainerManager::container()->class(static::class);
+        $class = MP::GET(static::class);
         return $class->__apply($func, $arguments);
     }
 
     function __apply($func, $arguments){
         if (method_exists($this, $func)){
             $method = new \ReflectionMethod($this, $func);
-            $parameters = $method->getParameters();
+            // $parameters = $method->getParameters();
 
             $method->setAccessible(true);
             return $method->invoke($this, ...$arguments);

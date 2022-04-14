c7b0fa3aa90d579454848d30f0f938635e86104d
picus23 <picus.2022@mail.ru>
2022-04-14 11:11:05 +0300

    props constructor

--- example/index.php
+++ example/index.php
@@ -8,12 +8,24 @@ use DI2\MP;
 class Test {
 	use Container;
 
-	private function print($text){
-		echo "$text\n";
+	private $alert = '';
+
+	function __construct($super, $alert, $hh) {
+		echo "alert: $alert, hh: $hh;\n";
+
+		$this->alert = $alert;
+	}
+
+	private function print($text) {
+		echo "$this->alert: $text\n";
 	}
 }
 
 
-echo MP::GET(Test::class, 'HEROS')->print('hello world');
+echo MP::GET(
+	class: Test::class, 
+	alias: 'HEROS', 
+	constructor: ['hh' => 'dss', 'alert' => 'method']
+)->print('hello world');
 
 echo Test::print('hello world');

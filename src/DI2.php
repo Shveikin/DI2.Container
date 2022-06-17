ae9db22b045fb911a9a32ec92a8f5a5c76ee32b1
Ilya Shveikin <no_soap_0@mail.ru>
2022-06-17 05:37:10 +0300

    read init merhods from parent

--- src/DI2.php
+++ src/DI2.php
@@ -2,36 +2,58 @@
 
 namespace DI2;
 
-trait DI2 {
-    public function __get($alias) {
+/**
+ * создайте trait для автоматической подгрузки свойств
+ * В trait укажите функцию с именем свойства
+ *  свойство создастся один раз для всех класов которые исользуют trait
+ *  если вы хотите чтобы для каждого класса свойство было уникальным
+ *  поставьте в начале функции знак "_"
+ */
+
+trait DI2
+{
+    public function __get($alias)
+    {
         if (!property_exists($this, $alias)) {
-            $uses = false;
-            foreach (class_uses($this) as $use) {
-                if (method_exists($use, $alias)){
+
+            foreach ($this->xmergeusemethods() as $use) {
+                if (method_exists($use, $alias)) {
                     $this->$alias = MP::DI2($this, $alias);
-                    $uses = true;
-                    break;
+                    return $this->$alias;
+                }
+
+                if (method_exists($use, "_$alias")) {
+                    $this->$alias = $this->{"_$alias"}();
+                    return $this->$alias;
                 }
             }
 
-            if (!$uses){
-                if (method_exists($this, '__anyKey')){
-                    return $this->__anyKey($alias);
-                }
+            if (method_exists($this, '__anyKey')) {
+                $this->$alias = $this->__anyKey($alias);
+                return $this->$alias;
             }
-            /* 
-            if (method_exists($this, $alias)){
-                $this->$alias = MP::DI2($this, $alias);
-            } else {
-                throw new \Exception(static::class . "->$alias - not exists ");
-            } 
-            */
         }
-        
+
         return $this->$alias;
     }
 
-    static function __init__($from, $alias){
+    private function xmergeusemethods()
+    {
+        foreach (class_uses($this) as $use) {
+            yield $use;
+        }
+
+        foreach (class_parents($this) as $parent) {
+            foreach (class_uses($parent) as $use) {
+                yield $use;
+            }
+        }
+
+        return false;
+    }
+
+    public static function __init__($from, $alias)
+    {
         return $from->{$alias}();
     }
 }
